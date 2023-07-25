99cd2910bc892c7c8947492bb0747bf705098982
FFengIll <fengyouzheng@gmail.com>
2023-07-25 18:16:57 +0800

    feat: add stores via pinia.

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,16 +1,19 @@
 // import './assets/main.css';
 
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import router from './router';
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(Antd)
-
 app.use(router)
 
 app.mount('#app')
