a5afca86e49a66a57187fa848b0ec875e4530800
FFengIll <fengyouzheng@gmail.com>
2023-07-25 18:16:51 +0800

    feat: add stores via pinia.

---
+++ front/src/stores/snapshot.ts
@@ -0,0 +1,79 @@
+import { defineStore } from 'pinia'
+import type { Message } from '../../sdk/models/message.js'
+
+export const useCounterStore = defineStore('counter', {
+    state: () => {
+        return { count: 0 }
+    },
+    // 也可以这样定义
+    // state: () => ({ count: 0 })
+    actions: {
+        increment() {
+            this.count++
+        },
+    },
+})
+
+export const useSnapshotStore = defineStore('snapshot', {
+    state: () => {
+        return {
+            snapshot:{
+            messages: [
+                {
+                    id: 1,
+                    role: 'user',
+                    content: "{{}}"
+                }
+            ]
+        },
+        debug: [
+                {
+                    id: 1,
+                    role: 'user',
+                    content: "{{}}"
+                },
+               
+            ],
+        compare:{
+            left: [
+                {
+                    id: 1,
+                    role: 'user',
+                    content: "test"
+                }
+            ],
+            right: [
+                {
+                    id: 1,
+                    role: 'user',
+                    content: "test"
+                }
+            ]
+        }
+
+        }
+    },
+    actions: {
+        sendToDebug(ms:Message[]) {
+            // var res  = ms.filter(item => {
+            //     return item.enable
+            // });
+            var res = ms
+            this.debug =  res
+        },
+        sendToCompareLeft(ms:Message[]) {
+            // var res  = ms.filter(item => {
+            //     return item.enable
+            // });
+            var res = ms
+            this.compare.right =  res
+        },
+        sendToCompareRight(ms:Message[]) {
+            // var res  = ms.filter(item => {
+            //     return item.enable
+            // });
+            var res = ms
+            this.compare.right =  res
+        },
+    },
+})
