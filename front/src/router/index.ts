dc67b935d3fa47d297d78eb387d4a63571608c24
FFengIll <fengyouzheng@gmail.com>
2023-07-25 18:17:34 +0800

    feat: update index.

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,42 +1,44 @@
 import PromptViewVue from '@/views/PromptView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import DebugView from "@/views/DebugView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    {
-        path: '/view/profile',
-        name: 'profile',
-        props: true,
-        component: () => import('../views/ProfileView.vue')
-      },
-      {
-        path: '/view/prompt/:key',
-        name: 'prompt',
-        component: PromptViewVue,
-        // props: route => ({ key: route.query.key }),
-        props:true
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            redirect: '/view/profile',
+        },
+        {
+            path: '/view/profile',
+            name: 'profile',
+            props: true,
+            component: () => import('../views/ProfileView.vue')
+        },
+        {
+            path: '/view/prompt/:key',
+            name: 'prompt',
+            component: PromptViewVue,
+            // props: route => ({ key: route.query.key }),
+            props: true
 
-      },
-      {
-        path: '/test',
-        name: 'test',
-        component: () => import('../views/DebugView.vue')
-      },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+        },
+        {
+            path: '/view/debug',
+            name: 'debug',
+            component: DebugView,
+            props: true,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue')
+        }
+    ]
 })
 
 export default router
