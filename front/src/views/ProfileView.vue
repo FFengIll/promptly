982ab1ac17ca68eadf62752586028d86511b9583
FFengIll <fengyouzheng@gmail.com>
2023-07-26 15:23:37 +0800

    refactor: update prompt view to vuejs composition.

--- front/src/views/ProfileView.vue
+++ front/src/views/ProfileView.vue
@@ -4,65 +4,51 @@
     <a-card title="Click to Open Prompt Detail">
         <a-list v-for="item in keys" :key="item" size="small">
             <a-list-item>
-                <a v-bind:href="handleButtonClick(item)">{{ item }}</a>
+                <a v-bind:href="generateHref(item)">{{ item }}</a>
             </a-list-item>
         </a-list>
 
     </a-card>
 </template>
   
-<script lang="ts">
+<script lang="ts" setup>
+import router from "@/router";
 import { DefaultApiFactory } from "../../sdk/apis/default-api";
+
+import { ref } from 'vue'
+
+const keys = ref([
+    '1', '2', '3'
+])
 
 const api = DefaultApiFactory(undefined, "http://localhost:8000")
 
-export default {
-    data() {
-        return {
+fetchList()
 
-            keys: [
-                '1', '2', '3'
-            ]
-        };
-    },
-    created() {
-        this.fetchList()
-    },
-    computed: {
-        api: () => api
-    },
-    methods: {
-        redirectToPage(key: string) {
-            this.$router.push('/view/prompt/' + key);
-        },
 
-        handleButtonClick(key: string) {
-            // 处理按钮点击事件
-            console.log("Button clicked for item with ID:", key);
 
-            // TODO: jump to another page /prompt/id
-            // this.redirectToPage(key)
+function generateHref(key: string) {
+    // 处理按钮点击事件
+    console.log("Button clicked for item with ID:", key);
 
-            return '/view/prompt/' + key
-        },
+    // TODO: jump to another page /prompt/id
+    // router.push('')
 
-        async fetchList() {
-            return this.api.listProfileProfileGet().then(
-                response => {
-                    console.log(response.data)
-                    this.keys = response.data.keys
-                    return response.data
-                }
-            ).catch(
-                error => {
-                    return {
-                        keys: [
-                            '1', '2', '3'
-                        ]
-                    };
-                }
-            )
+    return '/view/prompt/' + key
+}
+
+async function fetchList() {
+    return api.listProfileProfileGet().then(
+        response => {
+            console.log(response.data)
+            keys.value = response.data.keys
+            return response.data
         }
-    }
-};
+    ).catch(
+        error => {
+            console.error(error)
+        }
+    )
+}
+
 </script>
