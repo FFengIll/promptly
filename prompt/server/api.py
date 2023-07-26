9f7ee2704823253a2722fa527c2c4401ddbf07f1
FFengIll <fengyouzheng@gmail.com>
2023-07-26 15:05:48 +0800

    feat: update async api via httpx.

---
+++ prompt/server/api.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+
+import requests
+from sqlalchemy import func
+from sqlmodel import Session, select
+from httpx import AsyncClient
+import httpx
+from prompt.model.orm import Profile, engine
+from prompt.openai.config import key, url
+
+# create_db_and_tables()
+payload = {
+    "messages": [],
+    "key": key,
+    "model": "",
+}
+
+
+def new_profile(name):
+    with Session(engine) as s:
+        p = select([func.count(Profile.id)]).where(Profile.name == name)
+        if s.exec(p).one():
+            return
+        else:
+            p = Profile(name=name)
+            s.add(p)
+            s.commit()
+
+
+def list_profile():
+    res = []
+    with Session(engine) as s:
+        q = select(Profile)
+        for i in s.exec(q):
+            res.append(i.name)
+    return res
+
+
+def simple_chat(content):
+    messages = [dict(role="user", content=content)]
+    return chat(messages)
+
+
+async def chat(messages):
+    headers = {}
+
+    data = deepcopy(payload)
+    data["messages"] = messages
+
+    async with AsyncClient() as c:
+        response = await c.post(url, headers=headers, json=data, timeout=10)
+        answer = response.json()
+
+    return answer
