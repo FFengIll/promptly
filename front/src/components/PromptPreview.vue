1b4c75bb03d45417d5981cebda70d8198c5d8aad
FFengIll <fengyouzheng@gmail.com>
2023-07-28 19:51:20 +0800

    feat: add component for prompt preview.

---
+++ front/src/components/PromptPreview.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {useRouter} from 'vue-router';
+
+const router = useRouter()
+
+defineProps<{
+    messages: [{
+        role: string,
+        content: string,
+        enable: boolean,
+    }]
+}>()
+
+
+</script>
+
+<template>
+    <div v-for="item in messages">
+        <a-list-item v-if="item.enable">
+            <span :style="{ color: 'blue' }">{{ item.role }} </span><span>:&nbsp;</span>
+            <span style="white-space: pre-line"> {{ item.content }}</span>
+        </a-list-item>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
