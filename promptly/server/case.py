09ef75b4e7cf3ceb18f44d819050202b4c2ac3b2
FFengIll <fengyouzheng@gmail.com>
2023-08-08 15:10:23 +0800

    feat/backend: use mongo for case.

--- promptly/server/case.py
+++ promptly/server/case.py
@@ -1,93 +1,25 @@
-import copy
-from typing import List
-
+import fastapi
 import loguru
-from pydantic import BaseModel
 
 from promptly.manager.memory import CaseManager
 from promptly.model.case import Case
-from promptly.model.profile import Message, Snapshot
-from promptly.server import api
-from promptly.server.api import mongo
-from promptly.server.app import app
-from promptly.server.util import to_message
+from promptly.server.app import app, mongo
 
 log = loguru.logger
-
-manager = CaseManager("./profile/case")
-
-
-@app.get("/case")
-def list_case():
-    return [i.dict() for i in manager.values()]
+manager = mongo.case
 
 
-@app.get("/case/{key}")
-def get_case(key: int):
+@app.get("/api/case")
+def list_case(reload: bool):
+    if reload:
+        manager.reload()
+    return manager.keys()
+
+
+@app.get("/api/case/{key}")
+async def get_case(key: str):
     c: Case = manager.get(key)
+    log.info(c)
+    if not c:
+        raise fastapi.HTTPException(404)
     return c.dict()
-
-
-@app.get("/api/case/refresh")
-def refresh_case():
-    manager.refresh()
-
-
-class CaseResult(BaseModel):
-    id: int
-    source: str
-    target: str
-
-
-@app.post("/api/debug/loop")
-async def debug(count: int, messages: List[Message]):
-    res = []
-    for idx in range(count):
-        ms = to_message(messages)
-        response = await api.chat(ms)
-
-        mongo.push_history(Snapshot(prompt=ms, response=response))
-
-        target = response
-        res.append(CaseResult(source="", target=target, id=idx).dict())
-
-    return res
-
-
-async def batch_debug(messages, data):
-    res = []
-    for idx, source in enumerate(data):
-        replaced_ms = copy.deepcopy(messages)
-        for m in replaced_ms:
-            m.content = m.content.replace("{{}}", source)
-
-        ms = to_message(replaced_ms)
-
-        target = await api.chat(ms)
-        mongo.push_history(Snapshot(prompt=ms, response=target))
-
-        res.append(CaseResult(source=source, target=target, id=idx).dict())
-
-    return res
-
-
-class DebugRequestBody(BaseModel):
-    source: str
-    messages: List[Message]
-
-
-@app.post("/api/debug/source")
-async def debug(body: DebugRequestBody):
-    source: str = body.source
-    messages: List[Message] = body.messages
-    data = [source]
-
-    return await batch_debug(messages, data)
-
-
-@app.post("/api/debug/case")
-async def debug(case_id: int, messages: List[Message]):
-    case = manager.get(case_id)
-    data = case.data
-
-    return await batch_debug(messages, data)
