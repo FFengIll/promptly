a98df91590c7a73459a64162a64ae0517e98f59c
FFengIll <fengyouzheng@gmail.com>
2023-08-08 15:07:03 +0800

    feat/web: add css.

--- promptly-vue/src/main.ts
+++ promptly-vue/src/main.ts
@@ -1,19 +1,20 @@
-// import './assets/main.css';
-
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
+import {createPinia} from 'pinia';
+import {createApp} from 'vue';
 
 import App from './App.vue';
 import router from './router';
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import './assets/modifier.css'
+// import './assets/base.css';
 
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
-app.use(Antd)
 app.use(router)
+app.use(pinia)
+
+app.use(Antd)
 
 app.mount('#app')
