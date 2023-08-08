84ecf7e3e66ce26bc443e69eede475407badd4bc
FFengIll <fengyouzheng@gmail.com>
2023-08-08 15:05:17 +0800

    feat/web: better profile (list) view.

--- promptly-vue/src/views/ProfileView.vue
+++ promptly-vue/src/views/ProfileView.vue
@@ -1,31 +1,48 @@
+<style scoped>
+.large-font {
+  font-size: 200%
+}
+</style>
+
 <template>
+
+
+  <a-card title="Prompt List">
     <a-space direction="horizontal">
-        <a-button @click="refresh">
-            Refresh
-        </a-button>
 
 
-        <a-input-search
-            v-model:value="key"
-            placeholder="input search text"
-            size="large"
-            @search="create_profile"
-        >
-            <template #enterButton>
-                <a-button>Create</a-button>
-            </template>
-        </a-input-search>
+      <a-input-search
+          v-model:value="key"
+          placeholder="input search text"
+          size="large"
+          @search="create_profile"
+      >
+        <template #enterButton>
+          <a-button>Create</a-button>
+        </template>
+      </a-input-search>
+
+      <a-button @click="refresh">
+        Refresh
+      </a-button>
+
 
     </a-space>
 
-    <a-card title="Click to Open Prompt Detail">
-        <a-list v-for="item in keys" :key="item" size="small">
-            <a-list-item>
-                <a v-bind:href="generateHref(item)">{{ item }}</a>
-            </a-list-item>
-        </a-list>
+    <a-divider></a-divider>
 
-    </a-card>
+    <a-list :data-source="keys" size="small" :grid="{ gutter: 16, column: 4 }">
+      <template #renderItem="{ item }">
+        <a-list-item>
+          <a-typography-link class="large-font"
+                             v-bind:href="generateHref(item)">{{
+              item
+            }}
+          </a-typography-link>
+        </a-list-item>
+      </template>
+    </a-list>
+  </a-card>
 </template>
 
 <script lang="ts" setup>
@@ -37,7 +54,7 @@ const api = DefaultApiFactory(undefined, "http://localhost:8000")
 
 // field
 const keys = ref([
-    '1', '2', '3'
+  '1', '2', '3'
 ])
 
 const key = ref<string>("")
@@ -46,38 +63,38 @@ const key = ref<string>("")
 fetchList()
 
 function refresh() {
-    api.apiCaseRefreshGet()
-    fetchList()
+  api.apiCaseGet()
+  fetchList()
 }
 
 function create_profile(name: string) {
-    api.apiProfileKeyPut(name)
-    refresh()
+  api.apiProfileKeyPut(name)
+  refresh()
 }
 
 function generateHref(key: string) {
-    // 处理按钮点击事件
-    console.log("Button clicked for item with ID:", key);
+  // 处理按钮点击事件
+  console.log("Button clicked for item with ID:", key);
 
-    // TODO: jump to another page /prompt/id
-    // router.push('')
+  // TODO: jump to another page /prompt/id
+  // router.push('')
 
-    return '/view/prompt/' + key
+  return '/view/prompt/' + key
 }
 
 async function fetchList() {
-    return api.apiProfileGet().then(
-        response => {
-            console.log(response.data)
-            keys.value = response.data.keys
-            keys.value.sort()
-            return response.data
-        }
-    ).catch(
-        error => {
-            console.error(error)
-        }
-    )
+  return api.apiProfileGet().then(
+      response => {
+        console.log(response.data)
+        keys.value = response.data.keys
+        keys.value.sort()
+        return response.data
+      }
+  ).catch(
+      error => {
+        console.error(error)
+      }
+  )
 }
 
 </script>
