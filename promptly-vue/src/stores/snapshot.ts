827834148e1ada4ae08d58ea7363062933184d59
FFengIll <fengyouzheng@gmail.com>
2023-08-09 16:38:49 +0800

    bugfix/web: store missing without `useLocalStorage`.

--- promptly-vue/src/stores/snapshot.ts
+++ promptly-vue/src/stores/snapshot.ts
@@ -1,66 +1,50 @@
-import {defineStore} from 'pinia'
-import type {Message} from '../../sdk/models'
+import { useLocalStorage } from '@vueuse/core';
+import { defineStore } from 'pinia';
+import type { Message } from '../../sdk/models';
 
 export const useSnapshotStore = defineStore('snapshot', {
-    state: () => {
-        return {
-            source: {
+    getters: {
+
+    },
+
+    state: () => ({
+        source: useLocalStorage('source',
+            {
                 name: "",
                 messages: <Message[]>[
-                    {
-                        id: 1,
-                        role: 'user',
-                        content: "{{}}"
-                    },
-                ]
-            },
-            compare: {
-                left: {
-                    name: "",
-                    messages: <Message[]>[
-                        {
-                            id: 1,
-                            role: 'user',
-                            content: "test"
-                        }
-                    ]
-                },
-                right: {
-                    name: "",
-                    messages: <Message[]>[
-                        {
-                            id: 1,
-                            role: 'user',
-                            content: "test"
-                        }
-                    ]
-                }
-            }
 
-        }
-    },
+                ]
+            }),
+    })
+    ,
     actions: {
         sendSource(ms: Message[], source: string) {
             var res = ms.filter(item => {
                 return item.enable
             });
-            this.source.messages = res
-            this.source.name = source
+            this.$patch(
+                {
+                    source: {
+                        messages: res,
+                        name: source
+                    }
+                }
+            )
         },
 
-        sendToCompareLeft(ms: Message[]) {
-            // var res  = ms.filter(item => {
-            //     return item.enable
-            // });
-            var res = ms
-            this.compare.right.messages = res
-        },
-        sendToCompareRight(ms: Message[]) {
-            // var res  = ms.filter(item => {
-            //     return item.enable
-            // });
-            var res = ms
-            this.compare.right.messages = res
-        },
+        // sendToCompareLeft(ms: Message[]) {
+        //     // var res  = ms.filter(item => {
+        //     //     return item.enable
+        //     // });
+        //     var res = ms
+        //     this.compare.right.messages = res
+        // },
+        // sendToCompareRight(ms: Message[]) {
+        //     // var res  = ms.filter(item => {
+        //     //     return item.enable
+        //     // });
+        //     var res = ms
+        //     this.compare.right.messages = res
+        // },
     },
 })
