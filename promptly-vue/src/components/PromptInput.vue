255e162ba6393ca97a753876a31efc6f80eedbe5
FFengIll <fengyouzheng@gmail.com>
2023-08-10 12:42:48 +0800

    feat/web: more ui control for prompt input.

--- promptly-vue/src/components/PromptInput.vue
+++ promptly-vue/src/components/PromptInput.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 
-import {CloseOutlined, DownOutlined, PlusOutlined, UpOutlined} from "@ant-design/icons-vue";
+import { CloseOutlined, DownOutlined, PlusOutlined, UpOutlined } from "@ant-design/icons-vue";
 
-import {useClipboard} from '@vueuse/core';
-import {useRouter} from 'vue-router';
+import { useClipboard } from '@vueuse/core';
+import { useRouter } from 'vue-router';
 
-import type {Message} from "../../sdk";
+import type { Message } from "../../sdk";
 
 
-const {text, copy, copied, isSupported} = useClipboard({})
+const { text, copy, copied, isSupported } = useClipboard({})
 
 const router = useRouter()
 
@@ -22,8 +22,9 @@ const emit = defineEmits<{
 
 const props = defineProps<{
     messages: Message[],
-    mode?: string,
+    withControl?: boolean,
     withCopy?: boolean
+    withSidebar?: boolean
 }>(
 
 )
@@ -45,43 +46,59 @@ function remove(id) {
     emit('remove', id)
 }
 
+function color(role: string) {
+    switch (role) {
+        case 'user':
+            return { color: 'blue' }
+
+            break;
+        case 'system':
+            return { color: 'red' }
+
+        case 'assitant':
+            return { color: 'black' }
+            break
+        default:
+            break;
+    }
+}
+
 </script>
 
 <template>
-
     <a-card v-for="item in messages" :key="item.id">
         <!-- <template #extra><a href="#">more</a></template> -->
 
-        <a-row>
-            <a-divider v-if="mode!='simple'">
+        <a-row v-if="withControl">
+            <a-divider>
                 <a-space>
                     <a-button @click="add(item.id)">
                         <template #icon>
-                            <PlusOutlined/>
+                            <PlusOutlined />
                         </template>
                     </a-button>
 
                     <a-divider type="vertical"></a-divider>
 
                     <!-- up down the order -->
-                    <a-button type="primary" shape="round" @click="orderUp  (item.id, )">
+                    <a-button type="primary" shape="round" @click="orderUp(item.id,)">
                         <template #icon>
-                            <UpOutlined/>
+                            <UpOutlined />
                         </template>
                     </a-button>
-                    <a-button type="primary" shape="round" @click="orderDown(item.id, )">
+                    <a-button type="primary" shape="round" @click="orderDown(item.id,)">
                         <template #icon>
-                            <DownOutlined/>
+                            <DownOutlined />
                         </template>
                     </a-button>
 
                     <a-divider type="vertical"></a-divider>
 
                     <a-popconfirm title="Are you sure delete this task?" ok-text="Yes" cancel-text="No"
-                                  @confirm="remove(item.order)">
+                        @confirm="remove(item.order)">
                         <a-button>
                             <template #icon>
-                                <CloseOutlined/>
+                                <CloseOutlined />
                             </template>
                         </a-button>
                     </a-popconfirm>
@@ -95,13 +112,13 @@ function remove(id) {
 
             <!-- role -->
             <a-space direction="vertical">
-
-                <a-radio-group v-model:value="item.role" option-type="button" button-style="solid">
+                <a-radio-group v-if="!withSidebar" v-model:value="item.role" option-type="button" button-style="solid">
                     <a-radio-button value="user">User</a-radio-button>
                     <a-radio-button value="system">System</a-radio-button>
                     <a-radio-button value="assistant">Assitant</a-radio-button>
                 </a-radio-group>
             </a-space>
+            <a-divider type="vertical"></a-divider>
 
             <!-- role select -->
             <!-- <a-select ref="select" v-model:value="item.role" style="width: 120px">
@@ -117,9 +134,13 @@ function remove(id) {
 
             <!-- enable toggle -->
             <a-space direction="horizontal">
-                <span></span>
-                <a-typography-text>Enable</a-typography-text>
-                <a-switch v-model:checked="item.enable"></a-switch>
+                <a-button v-if="withSidebar" :style="color(item.role)">
+                    {{ item.role }}
+                </a-button>
+                <a-space v-if="withControl">
+                    <a-typography-text>Enable</a-typography-text>
+                    <a-switch v-model:checked="item.enable"></a-switch>
+                </a-space>
                 <a-button v-if="withCopy" @click="copy(JSON.stringify(item.content))">Copy JSON</a-button>
             </a-space>
         </a-row>
@@ -127,10 +148,8 @@ function remove(id) {
 
             <a-col :span='24'>
                 <!-- content edit -->
-                <a-textarea v-model:value="item.content"
-                            :auto-size="{ minRows: 3, maxRows: 5 }"
-                            placeholder="textarea with clear icon"
-                />
+                <a-textarea v-model:value="item.content" :auto-size="{ minRows: 3, maxRows: 5 }"
+                    placeholder="textarea with clear icon" />
             </a-col>
 
         </a-row>
@@ -139,6 +158,4 @@ function remove(id) {
     </a-card>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
