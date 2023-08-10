62b96d1dae89dd8b4b5d934312f96c7536e983a7
FFengIll <fengyouzheng@gmail.com>
2023-08-10 12:43:07 +0800

    feat/web: add ApiHelper.

--- promptly-vue/src/scripts/api.ts
+++ promptly-vue/src/scripts/api.ts
@@ -1,5 +1,36 @@
-import {DefaultApiFactory} from "../../sdk/apis/default-api";
+import type { Argument, Message } from "sdk/models";
+import * as defaultApi from "../../sdk/apis/default-api";
+import { format } from "./template";
 
 export function ApiFactory() {
-    return DefaultApiFactory(undefined, "http://localhost:8000")
+    return defaultApi.DefaultApiFactory(undefined, "http://localhost:8000")
+}
+
+
+const api = ApiFactory()
+
+export class ApiHelper {
+    static async doChat(key: string, messages: Message[], args: Argument[]) {
+        return await api.apiCommitArgsPost(args, key)
+            .then(
+                (response) => {
+
+                    console.log(messages)
+
+                    let ms = messages?.filter((item: Message) => item.enable)
+
+                    let another: Message[] = JSON.parse(JSON.stringify(ms))
+                    another.forEach((item) => {
+                        item.content = format(item.content, args)
+                    })
+                    console.log("will chat with messages", another)
+
+                    return api.apiChatPost(another).then(
+                        (response) => {
+                            return response
+                        }
+                    )
+                }
+            )
+    }
 }
