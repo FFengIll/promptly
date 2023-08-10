ac7a0f9ae1b1b1bdbe7324562d01eec3201ed00f
FFengIll <fengyouzheng@gmail.com>
2023-08-10 10:02:57 +0800

    feat/model: add md5 into Commit.

--- promptly/model/prompt.py
+++ promptly/model/prompt.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from typing import List, Any
 from uuid import uuid1
 
@@ -52,6 +53,18 @@ class Message(PromptItem):
 class Commit(BaseModel):
     messages: List[Message] = Field(default_factory=list)
     response: str = ""
+    md5: str = ""
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.md5 = self.calc_md5()
+
+    def calc_md5(self):
+        items = [m.json() for m in self.messages]
+        h = md5()
+        for it in items:
+            h.update(it.encode())
+        return h.hexdigest()
 
 
 class Argument(BaseModel):
@@ -64,6 +77,7 @@ class Project(BaseModel):
     name: str
     commits: List[Commit] = Field(default_factory=list)
     args: List[Argument] = Field(default_factory=list)
+    cases: List[List[Argument]] = Field(default_factory=list)
 
 
 @autocomplete
@@ -118,3 +132,15 @@ class Profile(BaseModel):
 def test_model():
     res = Profile.parse_obj(dict(name="", messages=[]))
     print(res)
+
+
+def test_json():
+    for i in range(5):
+        m = Message(role="1", content="123")
+        print(m.json())
+
+
+def test_commit():
+    for i in range(5):
+        c = Commit()
+        print(c.md5)
