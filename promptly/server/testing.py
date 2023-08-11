aad4cd474c8052aa7ecbd70d8a4c120c8155f533
FFengIll <fengyouzheng@gmail.com>
2023-08-11 11:36:10 +0800

    feat: new testing api.

--- promptly/server/testing.py
+++ promptly/server/testing.py
@@ -6,31 +6,27 @@ import loguru
 from pydantic import BaseModel
 
 from promptly.model.case import CaseResult
-from promptly.model.prompt import Message, Commit
+from promptly.model.prompt import Commit, Message
 from promptly.server import llm
-from promptly.server.app import app
-from promptly.server.app import mongo
+from promptly.server.app import app, mongo
 from promptly.server.llm import to_message
 
 log = loguru.logger
 
 
-class DebugRequestBody(BaseModel):
-    source: str
+class TestingRequestBody(BaseModel):
+    sources: List[str]
     messages: List[Message]
 
 
-@app.post("/api/testing/loop")
-async def do_test(count: int, messages: List[Message]):
+@app.post("/api/testing")
+async def run_test_with_source(body: TestingRequestBody, repeat: int = 1):
+    data = body.sources
+    messages: List[Message] = body.messages
+
     res = []
-    for idx in range(count):
-        ms = to_message(messages)
-        response = await llm.chat(ms)
-
-        mongo.history.push(Snapshot(prompt=ms, response=response))
-
-        target = response
-        res.append(CaseResult(source="", target=target, id=idx).dict())
+    for idx in range(repeat):
+        res += await batch_test(messages, data)
 
     return res
 
@@ -53,22 +49,3 @@ async def batch_test(messages, data):
         res.append(CaseResult(source=source, target=target, id=idx).dict())
 
     return res
-
-
-@app.post("/api/testing/source")
-async def run_test_with_source(body: DebugRequestBody):
-    source: str = body.source
-    messages: List[Message] = body.messages
-    data = [source]
-
-    return await batch_test(messages, data)
-
-
-@app.post("/api/testing/case")
-async def run_test_with_case(name: str, messages: List[Message]):
-    case = mongo.case.get(name)
-    if not case:
-        raise fastapi.HTTPException(404)
-    data = case.data
-
-    return await batch_test(messages, data)
