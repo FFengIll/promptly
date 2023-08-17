7e876714de3770ff879bda3eb7647ee5a012cbac
FFengIll <fengyouzheng@gmail.com>
2023-08-17 18:22:00 +0800

    feat: better CaseInput.

--- promptly-vue/src/components/CaseInput.vue
+++ promptly-vue/src/components/CaseInput.vue
@@ -1,41 +1,86 @@
 <script setup lang="ts">
+import type { ArgumentSetting } from 'sdk/models';
+import { ref } from 'vue';
+
 
 const props = defineProps<{
-    args: {
-        key: string, value: string
-    }[]
+    setting: ArgumentSetting,
+    args: Map<string, string>
+
 }>()
 
+const extra = ref(null)
+
+console.log(props.setting)
+
+const emit = defineEmits<{
+    (e: 'select', key: string, value: string): void
+}>()
+
+function onSelect(key, value) {
+    console.log(key, value)
+    emit('select', key, value)
+}
+
+
+function data() {
+    return [{ key: 1, values: 2 }]
+    return Object.keys(props.setting.args!!.keys).map(key => {
+        return {
+            key: key,
+            values: props.setting.args!![key]
+        }
+    }
+    )
+}
+
+
+const columns = [
+    {
+        name: 'Key',
+        dataIndex: 'key',
+        key: 'key',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'values',
+        key: 'values',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+    },
+];
 
 </script>
 
 <template>
-    <a-list item-layout="horizontal" :data-source="props.args">
+    <a-list item-layout="horizontal">
+        <!-- <a-table :columns="columns" :data-source="data"> -->
+        <!-- </a-table> -->
 
-        <template #renderItem="{ item }">
-            <a-list-item>
-                <!-- <template #actions>
-                    <a key="list-loadmore-edit">edit</a>
-                    <a key="list-loadmore-more">more</a>
-                </template> -->
-                <a-input-group>
-                    <a-space direction="horizontal">
-                        <a-typography-text>
-                            Key
-                        </a-typography-text>
-                        <a-input v-model:value="item.key"></a-input>
+        <a-list-item v-for="(values, key) in  setting.args " :key="key">
 
-                        <a-typography-text>
-                            Value
-                        </a-typography-text>
-                        <a-input v-model:value="item.value"></a-input>
+            <a-input-group>
+                <a-space direction="horizontal">
+                    <a-typography-text>
+                        {{ key }}
+                    </a-typography-text>
 
-                    </a-space>
+                    <a-select ref="select" :value="args.get(key)" style="width: 300px"
+                        @select="(value, option) => onSelect(key, value)">
+                        <a-select-option v-for=" (v, index) in values" :key="v">{{ v }}</a-select-option>
+                    </a-select>
 
-                </a-input-group>
-            </a-list-item>
-        </template>
+                </a-space>
+
+            </a-input-group>
+        </a-list-item>
+
+
     </a-list>
+
+    <slot name="extra"></slot>
 </template>
 
 <style scoped></style>
