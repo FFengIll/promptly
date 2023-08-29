b4ff0fce7f49e92c5915cc428f7f699ba66dbe5a
FFengIll <fengyouzheng@gmail.com>
2023-08-29 16:00:41 +0800

    feat: add model select.

---
+++ promptly-vue/src/components/ModelSelect.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{
+    model: string,
+}>()
+
+
+const emit = defineEmits<{
+    (e: 'select', value: string): void
+}>()
+
+function onSelect(value: string) {
+    console.log(value)
+    emit('select', value)
+}
+
+const models = [
+    "gpt-3.5-turbo",
+    'anthropic/claude-instant-v1'
+]
+
+</script>
+
+<template>
+    <a-select ref="select" v-model:value="props.model" style="width: 120px" @change="onSelect">
+        <a-select-option v-for="value in models" :value="value">{{ value }}</a-select-option>
+
+    </a-select>
+</template>
+
+<style scoped></style>
