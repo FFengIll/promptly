7d3174dba94eaf5f186fe9b9a356205dc3240052
FFengIll <fengyouzheng@gmail.com>
2023-09-04 21:20:28 +0800

    feat/web: support prompt group.

--- web/src/views/IndexView.vue
+++ web/src/views/IndexView.vue
@@ -27,15 +27,17 @@
 
         <a-divider></a-divider>
 
-        <a-list :data-source="keys" size="small" :grid="{ gutter: 16, column: 4 }">
-            <template #renderItem="{ item }">
-                <a-list-item>
-                    <a-typography-link class="large-font" @click="openPrompt(item)">
-                        {{ item }}
-                    </a-typography-link>
-                </a-list-item>
-            </template>
-        </a-list>
+        <a-card v-for="(values,key) in group" :title="key || '[default]'">
+            <a-list :data-source="values.sort()" size="small" :grid="{ gutter: 16, column: 4 }">
+                <template #renderItem="{ item }">
+                    <a-list-item>
+                        <a-typography-link class="large-font" @click="openPrompt(item)">
+                            {{ item }}
+                        </a-typography-link>
+                    </a-list-item>
+                </template>
+            </a-list>
+        </a-card>
     </a-card>
 </template>
 
@@ -49,18 +51,18 @@ import {onMounted, ref} from 'vue';
 
 
 // field
-const keys = ref([
-    '1', '2', '3'
-])
+const group = ref({
+    "": []
+})
 
 
 const store = useSnapshotStore()
 
 // created
 onMounted(
-    () => {
-        fetchList(false)
-    }
+() => {
+    fetchList(false)
+}
 )
 
 
@@ -77,16 +79,15 @@ async function create_profile(name: string) {
 
 async function fetchList(refresh: boolean) {
     return backend.apiPromptGet(refresh).then(
-        response => {
-            console.log(response.data)
-            keys.value = response.data.keys
-            keys.value.sort()
-            return response.data
-        }
+    response => {
+        group.value = response.data['data']
+        console.log(response.data)
+        return response.data
+    }
     ).catch(
-        error => {
-            console.error(error)
-        }
+    error => {
+        console.error(error)
+    }
     )
 }
 
