4c0b0177ff74605633ba943554a9e7064a4eabca
FFengIll <fengyouzheng@gmail.com>
2023-09-05 12:05:36 +0800

    refactor: simplify notice.

--- web/src/scripts/notice.ts
+++ web/src/scripts/notice.ts
@@ -1,10 +1,9 @@
 import { notification, type NotificationPlacement } from "ant-design-vue";
-import type { NotificationInstance } from "ant-design-vue/es/notification";
 
 const [notificationApi, contextHolder] = notification.useNotification();
 
 
-export const openNotification = (api: NotificationInstance, message: string, status: string) => {
+export const openNotification = (message: string, status: string) => {
     let placement: NotificationPlacement = 'bottomRight' as NotificationPlacement
 
     console.log(message, status)
