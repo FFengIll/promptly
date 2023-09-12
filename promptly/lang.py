dcbfa6d3bc72306e7f088f8e2966ee1e3b9cb2b1
FFengIll <fengyouzheng@gmail.com>
2023-09-12 17:30:24 +0800

    feat: add regular lang define.

---
+++ promptly/lang.py
@@ -0,0 +1,35 @@
+langs = [
+    {"locale": "id", "code": "Indonesian", "title": "Bahasa Indonesia"},
+    {"locale": "de", "code": "German", "title": "Deutsch"},
+    {"locale": "en", "code": "English", "title": "English"},
+    {"locale": "es", "code": "Spanish", "title": "Espa\xF1ol"},
+    {"locale": "fr", "code": "French", "title": "Fran\xE7ais"},
+    {"locale": "it", "code": "Italian", "title": "Italiano"},
+    {"locale": "pt_BR", "code": "Portuguese(Brasil)", "title": "Portugu\xEAs(BR)"},
+    {"locale": "pt_PT", "code": "Portuguese(Portugal)", "title": "Portugu\xEAs(PT)"},
+    {
+        "locale": "ru",
+        "code": "Russian",
+        "title": "\u0420\u0443\u0441\u0441\u043A\u0438\u0439",
+    },
+    {"locale": "vi", "code": "Vietnamese", "title": "Ti\u1EBFng Vi\u1EC7t"},
+    {"locale": "tr", "code": "Turkish", "title": "T\xFCrk\xE7e"},
+    {
+        "locale": "ar",
+        "code": "Arabic",
+        "title": "\u0627\u0644\u0639\u0631\u0628\u064A\u0629",
+    },
+    {"locale": "ko", "code": "Korean", "title": "\uD55C\uAD6D\uC5B4"},
+    {
+        "locale": "zh_TW",
+        "code": "Chinese (Traditional)",
+        "title": "\u4E2D\u6587\uFF08\u7E41\u9AD4\uFF09",
+    },
+    {
+        "locale": "zh_CN",
+        "code": "Chinese (Simplified)",
+        "title": "\u4E2D\u6587\uFF08\u7B80\u4F53\uFF09",
+    },
+    {"locale": "ja", "code": "Japanese", "title": "\u65E5\u672C\u8A9E"},
+]
+
