7e04bb2c5f056383deb1939e1b054168b4133c37
FFengIll <fengyouzheng@gmail.com>
2023-09-12 21:16:17 +0800

    feat: add store for global-config

---
+++ web/src/stores/global-config.ts
@@ -0,0 +1,25 @@
+import { useLocalStorage } from '@vueuse/core';
+import { defineStore } from 'pinia';
+import type { Argument, ArgumentSetting } from '../../sdk/models';
+
+export const useConfigStore = defineStore('config', {
+    getters: {},
+
+    state: () => ({
+        globalArgs: useLocalStorage('args',
+            <ArgumentSetting>{
+
+            }
+        ),
+    })
+    ,
+    actions: {
+        updateArgs(args: Argument[]) {
+            this.$patch(
+                {
+                    globalArgs: { args: args }
+                }
+            )
+        },
+    },
+})
