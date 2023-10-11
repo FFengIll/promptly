9d5d30b9631f8e21da48f59c8ee617b69b507371
FFengIll <fengyouzheng@gmail.com>
2023-10-11 16:50:45 +0800

    feat: add gpt-4 llm.

--- web/src/scripts/llm.ts
+++ web/src/scripts/llm.ts
@@ -1,9 +1,9 @@
-
 export const LLM = {
     "gpt-3.5-turbo": "gpt-3.5-turbo",
+    "gpt-4": "gpt-4",
+    'anthropic/claude-instant-v1': 'anthropic/claude-instant-v1',
     "gpt-3.5-turbo-0613": "gpt-3.5-turbo-0613",
     "gpt-4-0613": "gpt-4-0613",
-    'anthropic/claude-instant-v1': 'anthropic/claude-instant-v1',
     'codellama-7b': "local/codellama-7b",
 }
 export const defaultLLM = "gpt-3.5-turbo"
