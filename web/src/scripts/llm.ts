5ac841428e196cbb55e48d2fd338bd963d61b20f
FFengIll <fengyouzheng@gmail.com>
2023-10-12 16:59:16 +0800

    feat: add "gpt-3.5-turbo-instruct"

--- web/src/scripts/llm.ts
+++ web/src/scripts/llm.ts
@@ -1,7 +1,13 @@
 export const LLM = {
+    // openai
     "gpt-3.5-turbo": "gpt-3.5-turbo",
     "gpt-4": "gpt-4",
+    "gpt-3.5-turbo-instruct":"gpt-3.5-turbo-instruct",
+
+    // openrouter
     'anthropic/claude-instant-v1': 'anthropic/claude-instant-v1',
+
+    // special
     "gpt-3.5-turbo-0613": "gpt-3.5-turbo-0613",
     "gpt-4-0613": "gpt-4-0613",
     'codellama-7b': "local/codellama-7b",
