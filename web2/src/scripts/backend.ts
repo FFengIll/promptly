9a4d112c367e2c72ad1a5d111552a52f73a978ec
FFengIll <fengyouzheng@gmail.com>
2025-04-18 16:49:30 +0800

    bugfix: for linter problems.

--- web2/src/scripts/backend.ts
+++ web2/src/scripts/backend.ts
@@ -1,10 +1,10 @@
 import * as defaultApi from "../sdk/api";
-import type { Argument, LLMOption, Message } from "../sdk/models";
+import type { Argument, LLMOption, Message } from "../sdk";
 import { format } from "./template";
 
 
 function ApiFactory() {
-    let baseURL = "" // process.env.NEXT_PUBLIC_BASE_API;
+    const baseURL = import.meta.env.VITE_BASE_API
     return defaultApi.DefaultApiFactory(undefined, baseURL)
 }
 
@@ -12,7 +12,7 @@ export const backend = ApiFactory()
 
 export class BackendHelper {
     static replace(messages: Message[], args: Argument[]) {
-        let another: Message[] = JSON.parse(JSON.stringify(messages))
+        const another: Message[] = JSON.parse(JSON.stringify(messages))
         another.forEach((item) => {
             item.content = format(item.content, args)
             console.log(item.content)
@@ -26,9 +26,9 @@ export class BackendHelper {
         console.log("origin argument", args)
         console.log("origin options", options)
 
-        let enables = messages?.filter((item: Message) => item.enable)
+        const enables = messages?.filter((item: Message) => item.enable)
 
-        let another: Message[] = BackendHelper.replace(enables, args)
+        const another: Message[] = BackendHelper.replace(enables, args)
         console.log("will chat with messages", another)
 
         return backend.apiActionChatPost({
